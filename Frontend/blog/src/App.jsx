3086ade5a4f15a5219000e13d5c7288779cb7922
DevFaith <waitherafaith@outlook.com>
2024-10-21 21:41:08 +0300

    * chore(App.jsx): comment out unused import and route for Blogs component * feat(App.jsx): add new route for Example component at /blogs

--- Frontend/blog/src/App.jsx
+++ Frontend/blog/src/App.jsx
@@ -1,11 +1,11 @@
 import Header from './Components/Header'
 import Footer from './Components/Footer'
 import Home from './Pages/Home'
-import Blogs from './Pages/Blogs'
+// import Blogs from './Pages/Blogs'
 import Addblog from './Pages/Addblog'
 import Login from './Pages/Login'
 import Signup from './Pages/Signup'
-
+import Example from './Example'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -19,10 +19,11 @@ function App() {
       
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='/blogs' element={<Blogs />} />
+          {/* <Route path='/blogs' element={<Blogs />} /> */}
           <Route path='/addblog' element={<Addblog />} />
           <Route path='/login' element={<Login />} />
           <Route path='/signup' element={<Signup />} />
+          <Route path='/blogs' element={<Example />} />
         </Routes>
         <Footer />
       </BrowserRouter>

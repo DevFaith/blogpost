20d8b141abb5bddaf670e8055f2aecf650601fba
DevFaith <waitherafaith@outlook.com>
2024-10-21 21:40:45 +0300

    * fix(Index.js): import firebase admin from correct file path * feat(Index.js): add cors middleware for handling cross-origin requests

--- Backend/Index.js
+++ Backend/Index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import multer from "multer";
-import { db, storage } from "./firebase.js";
+import { db, storage } from "./Firebase/Admin.js";
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import {
     collection,
@@ -10,6 +10,7 @@ import {
     doc,
     deleteDoc,
 } from "firebase/firestore";
+import cors from "cors"; // same port number 
 
 const app = express();
 const port = 4000;
@@ -17,22 +18,24 @@ const port = 4000;
 // middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 const upload = multer({ storage: multer.memoryStorage() }); // uploading images
-
+app.use(cors())
 // routes
 // getting/reading the blogs
 app.get("/blogs", async (req, res) => {
     try {
         // reading
-        const snapShot = await getDocs(collection(db, "BLOGS"));
+        const snapShot = await getDocs(collection(db, "blogs"));
         const blogs = [];
         snapShot.docs.forEach((doc) => {
             const data = doc.data();
             blogs.push({
                 id: doc.id,
-                title: data.title,
-                content: data.content,
+                title: data.name,
+                content: data.description,
                 imageurl: data.ImageUrl,
             });
+            console.log(data);
+
         });
         res.json({ blogs });
     } catch (error) {
@@ -42,8 +45,8 @@ app.get("/blogs", async (req, res) => {
 
 // adding Blog
 app.post("/addblog", upload.single("image"), async (req, res) => {
-    const title = req.body.title;
-    const content = req.body.content;
+    const title = req.body.name;
+    const content = req.body.description;
 
     // file multer
     const file = req.file;
@@ -51,12 +54,12 @@ app.post("/addblog", upload.single("image"), async (req, res) => {
     try {
         // firebase firestore storage
         // logic to write file to storage
-        const refFolder = ref(storage, "Photos/" + file.originalname);
+        const refFolder = ref(storage, "blogImages/" + file.originalname);
         await uploadBytes(refFolder, file.buffer, { contentType: file.mimetype });
         const ImageUrl = await getDownloadURL(refFolder);
 
         //  write to database
-        await addDoc(collection(db, "BLOGS"), { title, content, ImageUrl });
+        await addDoc(collection(db, "blogs"), { title, content, ImageUrl });
         res.status(201).send("Post created successfully");
     } catch (error) {
         res.status(500).send("Error submitting" + error.message);
@@ -67,7 +70,7 @@ app.post("/addblog", upload.single("image"), async (req, res) => {
 app.delete("/blogs/:id", async (req, res) => {
     const postId = req.params.id;
     try {
-        const postRef = doc(db, "BLOGS", postId);
+        const postRef = doc(db, "blogs", postId);
         await deleteDoc(postRef);
         res.send("Post Deleted");
     } catch (error) {

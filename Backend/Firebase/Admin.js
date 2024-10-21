df34f63b9556728ecd30b5b373192b0daa1de64a
DevFaith <waitherafaith@outlook.com>
2024-10-21 21:41:36 +0300

    * feat(Firebase/Admin.js): add Firebase initialization and export database, storage, and auth instances

--- Backend/Firebase/Admin.js
+++ Backend/Firebase/Admin.js
@@ -1,1 +1,23 @@
-// admin implementation
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBHzqKr-dhfSnA2v6S4gO52R8rtToUouHk",
+    authDomain: "blog1011.firebaseapp.com",
+    databaseURL: "https://blog1011-default-rtdb.firebaseio.com",
+    projectId: "blog1011",
+    storageBucket: "blog1011.appspot.com",
+    messagingSenderId: "701407356867",
+    appId: "1:701407356867:web:5651a7671438f64395ff76",
+    measurementId: "G-011PWKCELC"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const storage = getStorage(app);
+const auth = getAuth(app);
+
+export { db, storage, auth };
